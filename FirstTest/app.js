ddf78e0d8fc20f5baf94e6f2a6df3c64c35f6dec
Vijay Vora <Vijay.Vora@glaston.net>
2021-08-02 08:46:21 +0200

    External files changes

--- FirstTest/app.js
+++ FirstTest/app.js
@@ -1,2 +1,6 @@
 
     document.write("Hello Vijay from India, I want to check If this External file works or not?");
+    
+    function myFunction() {
+        document.getElementById("demo").innerHTML="Paragraph changed.";
+    }
